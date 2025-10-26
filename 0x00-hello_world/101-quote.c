bb31ced49ba85b9cf969f728148306210ad751c3
Olalekeoluwaseyi <tutorialolalekeoluwaseyi@gmail.com>
2025-10-26 06:07:59 +0100

    writing to standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+
+int main() {
+    char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+    (void)write(2, quote, sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19/n")-1);
+    return (1);
+
+
+}
