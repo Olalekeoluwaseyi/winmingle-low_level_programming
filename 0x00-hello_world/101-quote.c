a97a9ead5820298f3d9f64cd51a397088e686b74
Olalekeoluwaseyi <tutorialolalekeoluwaseyi@gmail.com>
2025-10-26 06:38:01 +0100

    new line character added to the end the print out

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,9 @@
 #include <unistd.h>
 
 int main() {
-    char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+    char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-    (void)write(2, quote, sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19/n")-1);
+    (void)write(2, quote, sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n")-1);
     return (1);
 
 
